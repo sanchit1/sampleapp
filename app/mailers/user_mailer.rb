f2e6848c4372e64a9e58c019b32369eb2db93973
Sanchit <sanchitg@iitk.ac.in>
2011-06-05 17:04:11 -0700

    done with user profile1

--- app/mailers/user_mailer.rb
+++
@@ -1,6 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "sanchitgupta.sg007@gmail.com"
-def registration_confirmation(user)
-mail(:to => user.email, :subject => "Registered")
-end
-end
